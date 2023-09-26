bd1ba492bbd9867e5c38b6a1ab8eaeb016497364
FASIL K <fasilmlm10@gmail.com>
2023-09-26 17:46:19 +0530

    done netflix clone

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import './Banner.css'
+import React, { useEffect, useState } from 'react'
+import axios from '../../axios'
+import {API_KEY,imageUrl} from '../../constants/constants'
+import "./Banner.css"
 function Banner() {
+    const [movie,setMovie]=useState()
+    useEffect(()=>{
+        axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+            console.log(response.data.results[0])
+            setMovie(response.data.results[2])
+        })
+    },[])
   return (
-    <div className='banner' >
-        <div className='content'>
-            <h1 className='title'>Movie Name</h1>
-            <div className='banner_buttons' >
-                <button className='button'>Play</button>
-                <button className='button'>My list</button>
-            </div>
-            <h1 className='discription' >In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without </h1>
+    <div style={{backgroundImage:`url( ${movie ? imageUrl+movie.backdrop_path : ''})`}} className='banner'>
+      <div className='content'>
+        <h1 className='title'>{movie ? movie.title : ''}</h1>
+        <div className='banner_buttons'>
+            <button className='button'>Play</button>
+            <button className='button'>My list</button>
         </div>
-        <div className="fade_bottom"></div>      
+        <h2 className='description'>{movie ? movie.overview : ''} </h2>
+      </div>
+      <div className="fade_bottom"></div>
     </div>
   )
 }
---
+++ src/constants/constants.jsx
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY ='1e3fb2078d3deefeead48624c852ac8c';
+export  const imageUrl = 'https://image.tmdb.org/t/p/original';
